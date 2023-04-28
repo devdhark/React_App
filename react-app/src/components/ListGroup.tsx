190733cc1bf2ef4185b73cdc02d772258b4703b5
devdhark <devdhar.kodiatar@gmail.com>
2023-04-27 22:27:51 -0500

    ListGroup component

---
+++ react-app/src/components/ListGroup.tsx
@@ -0,0 +1,22 @@
+function ListGroup() {
+  let items = ["New York", "Chicago", "Tokyo", "Atlanta", "Dallas"];
+  items = [];
+
+  const getMessage = () => {
+    return items.length === 0 && <p>No item found</p>;
+  };
+
+  return (
+    <>
+      <h1>List</h1>
+      {getMessage()}
+      <ul className="list-group">
+        {items.map((item) => (
+          <li key={item}>{item}</li>
+        ))}
+      </ul>
+    </>
+  );
+}
+
+export default ListGroup;
