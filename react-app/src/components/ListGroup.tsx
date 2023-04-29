700f65854f0b227c57448ea6b6249a9eb0001641
devdhark <devdhar.kodiatar@gmail.com>
2023-04-29 17:37:41 -0500

    event handlers

--- react-app/src/components/ListGroup.tsx
+++ react-app/src/components/ListGroup.tsx
@@ -1,18 +1,33 @@
+import { MouseEvent } from "react";
+
 function ListGroup() {
   let items = ["New York", "Chicago", "Tokyo", "Atlanta", "Dallas"];
-  items = [];
+  let selectedIndex = 0;
 
   const getMessage = () => {
     return items.length === 0 && <p>No item found</p>;
   };
+
+  // Event Handler
+  const handleClick = (event: MouseEvent) => console.log(event);
 
   return (
     <>
       <h1>List</h1>
       {getMessage()}
       <ul className="list-group">
-        {items.map((item) => (
-          <li key={item}>{item}</li>
+        {items.map((item, index) => (
+          <li
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            key={item}
+            onClick={handleClick}
+          >
+            {item}
+          </li>
         ))}
       </ul>
     </>
