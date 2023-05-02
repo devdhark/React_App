7597517a7cd6592c57cbd07131b2c94788d9ea99
devdhark <devdhar.kodiatar@gmail.com>
2023-05-01 19:44:49 -0500

    button

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,12 +1,13 @@
 import ListGroup from "./components/ListGroup";
 import Alert from "./components/Alert";
+import Button from "./components/Button";
 
 function App() {
   return (
     <div>
-      <Alert>
-        Hello <span>World</span>
-      </Alert>
+      <Button color="success" onClick={() => console.log("Clicked")}>
+        My Button
+      </Button>
     </div>
   );
 }
