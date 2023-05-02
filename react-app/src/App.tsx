8adbc6e4bb8a535d550c35bd47e656ffaba9aee8
devdhark <devdhar.kodiatar@gmail.com>
2023-05-01 20:16:10 -0500

    added alert

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,13 +1,21 @@
+import { useState } from "react";
 import ListGroup from "./components/ListGroup";
 import Alert from "./components/Alert";
 import Button from "./components/Button";
 
 function App() {
+  const [alertVisible, setAlertVisible] = useState(false);
+
   return (
     <div>
-      <Button color="success" onClick={() => console.log("Clicked")}>
-        My Button
-      </Button>
+      {
+        <Button color="success" onClick={() => setAlertVisible(true)}>
+          My Button
+        </Button>
+      }
+      {alertVisible && (
+        <Alert onClose={() => setAlertVisible(false)}>My alert</Alert>
+      )}
     </div>
   );
 }
--- react-app/src/components/Alert.tsx
+++ react-app/src/components/Alert.tsx
@@ -2,10 +2,25 @@ import { ReactNode } from "react";
 
 interface AlertProps {
   children: ReactNode;
+  onClose: () => void;
 }
 
-const Alert = ({ children }: AlertProps) => {
-  return <div className="alert alert-primary">{children}</div>;
+const Alert = ({ children, onClose }: AlertProps) => {
+  return (
+    <div
+      className="alert alert-warning alert-dismissible fade show"
+      role="alert"
+    >
+      <strong>My Alert</strong> Message
+      <button
+        type="button"
+        className="btn-close"
+        data-bs-dismiss="alert"
+        aria-label="Close"
+        onClick={onClose}
+      ></button>
+    </div>
+  );
 };
 
 export default Alert;
