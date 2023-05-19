b83a7bcdb6ee56282d693c7c9d577711b647ed03
devdhark <devdhar.kodiatar@gmail.com>
2023-05-18 22:29:44 -0500

    state management

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -12,30 +12,19 @@ function App() {
     console.log(item);
   };
 
+  const [person, setPerson] = useState({
+    firstName: "",
+    lastName: "",
+  });
+
+  const [isLoading, setLoading] = useState(false);
+  const fullName = firstName + " " + lastName;
+
   let items = ["New York", "Chicago", "Tokyo", "Atlanta", "Dallas"];
 
   return (
     <>
-      <div>
-        {<Button onClick={() => setAlertVisible(true)}>My Button</Button>}
-        {alertVisible && (
-          <Alert onClose={() => setAlertVisible(false)}>My alert</Alert>
-        )}
-      </div>
-
-      <div>
-        <ListGroup
-          items={items}
-          heading="Cities"
-          onSelectItem={handleSelectItem}
-        />
-      </div>
-      <div>
-        <BsFillCalendarFill color="red" size="40" />
-      </div>
-      <div>
-        <Like onClick={() => console.log("clicked")}></Like>
-      </div>
+      <div>{fullName}</div>
     </>
   );
 }
