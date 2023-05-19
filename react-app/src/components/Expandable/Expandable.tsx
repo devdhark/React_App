896790a2f65ea93e3089990c12957fb58075cf60
devdhark <devdhar.kodiatar@gmail.com>
2023-05-19 09:27:32 -0500

    expandable text

--- react-app/src/components/Expandable/Expandable.tsx
+++ react-app/src/components/Expandable/Expandable.tsx
@@ -1,19 +1,36 @@
 import React, { useState } from "react";
+import styles from "./Expandable.module.css";
 
 interface Props {
   MaxChars: number;
+  children: string;
 }
 
-const Expandable = ({ MaxChars }: Props) => {
+const Expandable = ({ MaxChars, children }: Props) => {
   const [more, setMore] = useState(false);
 
   const handleMoreClick = () => {
     setMore(!more);
   };
 
+  const expandableText = () => {
+    if (!more) {
+      return <p>{children.slice(0, MaxChars)}</p>;
+    }
+    return children;
+  };
+
+  const moreLessText = () => {
+    if (!more) {
+      return <p>More</p>;
+    }
+    return <p>Less</p>;
+  };
+
   return (
     <>
-      <button onClick={handleMoreClick}>More</button>
+      {expandableText()}
+      <button onClick={handleMoreClick}>{moreLessText()}</button>
     </>
   );
 };
