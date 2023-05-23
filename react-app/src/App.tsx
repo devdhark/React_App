c9f1b01ced045efdd89855ab2ecd3d5983962505
devdhark <devdhar.kodiatar@gmail.com>
2023-05-23 00:14:08 -0500

    create ExpenseList component

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,10 +1,45 @@
 import { useState } from "react";
 import Form from "./components/Form/Form";
+import ExpenseList from "./expense-tracker/components/ExpenseList";
 
 function App() {
+  const [expenses, setExpenses] = useState([
+    {
+      id: 1,
+      description: "expense1",
+      amount: 10,
+      category: "category1",
+    },
+    {
+      id: 2,
+      description: "expense2",
+      amount: 45,
+      category: "category2",
+    },
+    {
+      id: 3,
+      description: "expense3",
+      amount: 67,
+      category: "category1",
+    },
+    {
+      id: 4,
+      description: "expense4",
+      amount: 23,
+      category: "category2",
+    },
+  ]);
+
+  const onDelete = (id: number) => {
+    setExpenses(expenses.filter((e) => e.id !== id));
+  };
+
   return (
     <div>
-      <Form></Form>
+      <ExpenseList
+        expenses={expenses}
+        onDelete={(id) => onDelete(id)}
+      ></ExpenseList>
     </div>
   );
 }
--- react-app/src/components/Form/Form.tsx
+++ react-app/src/components/Form/Form.tsx
@@ -4,10 +4,11 @@ import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 
 const schema = z.object({
-  name: z.string().min(3, { message: "Name must be atleast 3 characters." }),
-  age: z
-    .number({ invalid_type_error: "Age field is required." })
-    .min(18, { message: "Age must be atleast 18." }),
+  description: z
+    .string()
+    .min(3, { message: "Description must be atleast 3 characters." }),
+  amount: z.number({ invalid_type_error: "Amount field is required." }),
+  category: z.string({ invalid_type_error: "Category field is required." }),
 });
 
 type FormData = z.infer<typeof schema>;
@@ -24,30 +25,52 @@ const Form = () => {
   return (
     <form onSubmit={handleSubmit((data) => console.log(data))}>
       <div className="mb-3">
-        <label htmlFor="name" className="form-label">
-          Name
+        <label htmlFor="description" className="form-label">
+          Description
         </label>
         <input
-          {...register("name")}
-          id="name"
+          {...register("description")}
+          id="description"
           type="text"
           className="form-control"
         />
-        {errors.name && <p className="text-danger">{errors.name.message}</p>}
+        {errors.description && (
+          <p className="text-danger">{errors.description.message}</p>
+        )}
       </div>
       <div className="mb-3">
-        <label htmlFor="age" className="form-label">
-          Age
+        <label htmlFor="amount" className="form-label">
+          Amount
         </label>
         <input
-          {...register("age", { valueAsNumber: true })}
-          id="age"
+          {...register("amount", { valueAsNumber: true })}
+          id="amount"
           type="number"
           className="form-control"
         />
-        {errors.age && <p className="text-danger">{errors.age.message}</p>}
+        {errors.amount && (
+          <p className="text-danger">{errors.amount.message}</p>
+        )}
       </div>
-      <button disabled={!isValid} className="btn btn-primary" type="submit">
+      <div className="mb-3">
+        <label htmlFor="category" className="form-label">
+          Category
+        </label>
+        <input
+          {...register("category")}
+          id="category"
+          type="text"
+          list="categories"
+          className="form-control"
+        />
+        <datalist id="categories">
+          <option value="a"></option>
+        </datalist>
+        {errors.category && (
+          <p className="text-danger">{errors.category.message}</p>
+        )}
+      </div>
+      <button className="btn btn-primary" type="submit">
         Submit
       </button>
     </form>
