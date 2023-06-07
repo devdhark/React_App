42a612f19c01f6e74924a0398e6e54860cabb7a5
devdhark <devdhar.kodiatar@gmail.com>
2023-06-06 20:34:03 -0500

    use effect state hook

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,71 +1,22 @@
-import { useState } from "react";
-import Form from "./components/Form/Form";
-import ExpenseList from "./expense-tracker/components/ExpenseList";
-import ExpenseFilter from "./expense-tracker/components/ExpenseFilter";
-import ExpenseForm from "./expense-tracker/components/ExpenseForm";
-import categories from "./expense-tracker/categories";
+import React, { useEffect, useRef } from "react";
 
-function App() {
-  const [selectedCategory, setSelectedCategory] = useState("");
+const App = () => {
+  const ref = useRef<HTMLInputElement>(null);
 
-  const [expenses, setExpenses] = useState([
-    {
-      id: 1,
-      description: "expense1",
-      amount: 10,
-      category: "category1",
-    },
-    {
-      id: 2,
-      description: "expense2",
-      amount: 45,
-      category: "Entertainment",
-    },
-    {
-      id: 3,
-      description: "expense3",
-      amount: 67,
-      category: "Entertainment",
-    },
-    {
-      id: 4,
-      description: "expense4",
-      amount: 23,
-      category: "Groceries",
-    },
-  ]);
+  // afterRender
+  useEffect(() => {
+    if (ref.current) ref.current.focus();
+  });
 
-  const onDelete = (id: number) => {
-    setExpenses(expenses.filter((e) => e.id !== id));
-  };
-
-  const visibleExpenses = selectedCategory
-    ? expenses.filter((e) => e.category === selectedCategory)
-    : expenses;
+  useEffect(() => {
+    document.title = "My App";
+  });
 
   return (
     <div>
-      <div className="mb-5">
-        <ExpenseForm
-          onSubmit={(expense) =>
-            setExpenses([
-              ...expenses,
-              { ...expense, id: expenses[expenses.length - 1].id + 1 },
-            ])
-          }
-        />
-      </div>
-      <div className="mb-3">
-        <ExpenseFilter
-          onSelectCategory={(category) => setSelectedCategory(category)}
-        ></ExpenseFilter>
-      </div>
-      <ExpenseList
-        expenses={visibleExpenses}
-        onDelete={(id) => onDelete(id)}
-      ></ExpenseList>
+      <input ref={ref} type="text" className="form-control" />
     </div>
   );
-}
+};
 
 export default App;
---
+++ react-app/src/App_expense_tracker.tsx
@@ -0,0 +1,71 @@
+import { useState } from "react";
+import Form from "./components/Form/Form";
+import ExpenseList from "./expense-tracker/components/ExpenseList";
+import ExpenseFilter from "./expense-tracker/components/ExpenseFilter";
+import ExpenseForm from "./expense-tracker/components/ExpenseForm";
+import categories from "./expense-tracker/categories";
+
+function App() {
+  const [selectedCategory, setSelectedCategory] = useState("");
+
+  const [expenses, setExpenses] = useState([
+    {
+      id: 1,
+      description: "expense1",
+      amount: 10,
+      category: "category1",
+    },
+    {
+      id: 2,
+      description: "expense2",
+      amount: 45,
+      category: "Entertainment",
+    },
+    {
+      id: 3,
+      description: "expense3",
+      amount: 67,
+      category: "Entertainment",
+    },
+    {
+      id: 4,
+      description: "expense4",
+      amount: 23,
+      category: "Groceries",
+    },
+  ]);
+
+  const onDelete = (id: number) => {
+    setExpenses(expenses.filter((e) => e.id !== id));
+  };
+
+  const visibleExpenses = selectedCategory
+    ? expenses.filter((e) => e.category === selectedCategory)
+    : expenses;
+
+  return (
+    <div>
+      <div className="mb-5">
+        <ExpenseForm
+          onSubmit={(expense) =>
+            setExpenses([
+              ...expenses,
+              { ...expense, id: expenses[expenses.length - 1].id + 1 },
+            ])
+          }
+        />
+      </div>
+      <div className="mb-3">
+        <ExpenseFilter
+          onSelectCategory={(category) => setSelectedCategory(category)}
+        ></ExpenseFilter>
+      </div>
+      <ExpenseList
+        expenses={visibleExpenses}
+        onDelete={(id) => onDelete(id)}
+      ></ExpenseList>
+    </div>
+  );
+}
+
+export default App;
