391bb545ad03987280e5398c22e8386fc103780b
devdhark <devdhar.kodiatar@gmail.com>
2023-06-06 21:05:11 -0500

    effect hook in products

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,20 +1,23 @@
-import React, { useEffect, useRef } from "react";
-
+import React, { useEffect, useRef, useState } from "react";
+import ProductList from "./expense-tracker/components/ProductList";
 const App = () => {
-  const ref = useRef<HTMLInputElement>(null);
-
+  const [category, setCategory] = useState("");
   // afterRender
-  useEffect(() => {
-    if (ref.current) ref.current.focus();
-  });
-
   useEffect(() => {
     document.title = "My App";
   });
 
   return (
     <div>
-      <input ref={ref} type="text" className="form-control" />
+      <select
+        className="form-select"
+        onChange={(event) => setCategory(event.target.value)}
+      >
+        <option value=""></option>
+        <option value="Clothing">Clothing</option>
+        <option value="Household">Household</option>
+      </select>
+      <ProductList category={category} />
     </div>
   );
 };
