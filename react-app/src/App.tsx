494fb3c1a270a13c439bda5c45255481d00e6ae4
devdhark <devdhar.kodiatar@gmail.com>
2023-06-18 13:52:06 -0500

    update using patch

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -59,6 +59,23 @@ const App = () => {
       });
   };
 
+  const updateUser = (user: User) => {
+    const originalUsers = [...users];
+    const updatedUser = {
+      ...user,
+      name: user.name + "!",
+    };
+
+    setUsers(users.map((u) => (u.id === user.id ? updatedUser : u)));
+
+    axios
+      .patch("https://jsonplaceholder.typicode.com/users/", +user.id)
+      .catch((err) => {
+        setError(err.message);
+        setUsers(originalUsers);
+      });
+  };
+
   return (
     <>
       {error && <p className="text-danger">{error}</p>}
@@ -73,12 +90,20 @@ const App = () => {
             className="list-group-item d-flex justify-content-between"
           >
             {user.name}
-            <button
-              className="btn btn-outline-danger"
-              onClick={() => deleteUser(user)}
-            >
-              Delete
-            </button>
+            <div>
+              <button
+                className="btn btn-outline-secondary mx-1"
+                onClick={() => updateUser(user)}
+              >
+                Update
+              </button>
+              <button
+                className="btn btn-outline-danger"
+                onClick={() => deleteUser(user)}
+              >
+                Delete
+              </button>
+            </div>
           </li>
         ))}
       </ul>
